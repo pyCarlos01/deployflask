ea028919d3ea242b28a5aa0c18af452b84f5bc42
Carlos Damião <136815475+pyCarlos01@users.noreply.github.com>
2023-10-20 10:06:33 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,43 +1,27 @@
 import re
-
-from flask import Flask, escape, request,render_template
-
+from flask import Flask, jsonify, request
+import ssl
+import json
 app = Flask(__name__)
 
-usuario = 'ubuntu'
-projeto = 'deployflask'
-git = 'https://github.com/Aguape21/deployflask.git'
-enderecos = 'deployflask.e-li.me\ndeployflask2.e-li.me'
-
-@app.route('/', )
-def comeco():
-
-    global usuario, projeto, git, enderecos
-
-    return render_template("comeco.html",
-                           usuario=usuario,
-                           projeto=projeto,
-                           git=git,
-                           enderecos=enderecos)
-
-@app.route('/tutorial', methods=['GET', 'POST'])
-def tutorial():
-
-    global usuario, projeto, git, enderecos
-
-    if request.method == 'POST':
-        form = request.form.to_dict()
-        usuario =   form['usuario'] if form.get('usuario') else ''
-        projeto =   form['projeto'] if form.get('projeto') else ''
-        git =       form['git'] if form.get('git') else ''
-        enderecos = form['enderecos'] if form.get('enderecos') else ''
+@app.route("/", methods=["POST"])
+def imprimir():
+  response = {"status": 200}
+  return jsonify(response)
 
 
-    return render_template("tutorial.html",
-                           usuario=usuario,
-                           projeto=projeto,
-                           git=git,
-                           enderecos=re.sub(r'\s+', ' ', enderecos).strip().split(' '))
+@app.route("/pix", methods=["POST"])
+def imprimirPix():
+  imprime = print(request.json)
+  data = request.json
+  with open('data.txt', 'a') as outfile:
+      outfile.write("\n")
+      json.dump(data, outfile)
+  return jsonify(imprime)
+
+@app.route("/test", methods=['GET', 'POST'])
+def test():
+    return 'CARLOS O MAIS GATO!'
 
 if __name__ == '__main__':
     app.run()
